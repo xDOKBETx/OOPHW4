a55899ca99c6a3000c538257d26ed46d72acfd93
Aleksey Valentinov <ds83351@chebnet.com>
2023-01-10 23:35:47 +0300

    Add Author

---
+++ Author.java
@@ -0,0 +1,31 @@
+package HW4;
+
+public class Author {
+    String name;
+    String surname;
+    String position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
